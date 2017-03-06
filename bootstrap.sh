6fab8085b9d198f38d4dc18e9c949a6fb74a324d
xohozu <xohozu@gmail.com>
2017-03-06 08:36:33 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,45 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+function install_dotfiles() {
+	echo "Updating .dotfiles in `pwd` ...";
+	git pull origin master;
+}
+
+function install_vimrc() {
+	if ! (cd ~/.vim_runtime && \
+	      echo "Updating .vimrc in `pwd` ..." && \
+		  git pull --rebase) then
+		echo "Installing .vimrc ...";
+		git clone https://github.com/amix/vimrc.git ~/.vim_runtime;
+		sh ~/.vim_runtime/install_awesome_vimrc.sh;
+	fi;
+}
+
+function pre_installs() {
+	installs=( install_dotfiles install_vimrc )
+	for install in "${installs[@]}" 
+	do
+		echo "";
+		`expr $install`;
+	done
+	echo ""
+}
 
 function bootstrap() {
+	pre_installs;
+
+	echo "Bootstraping ...";
+
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE" \
-		-avh --no-perms . ~;
+		-avhR --no-perms . ~;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
