983978547ea9929d3065e5ec6200d217762f8fa0
Qinghua Hong <xohozu@gmail.com>
2021-01-18 14:22:14 +0800

    Install gh

--- bootstrap.sh
+++ bootstrap.sh
@@ -117,7 +117,7 @@ function setup_dotfiles() {
 # cli
 
 function setup_cli_tools() {
-    brew_install autojump tldr rg fd fzf tig
+    brew_install autojump tldr rg fd fzf tig gh
 }
 
 # pre_install
