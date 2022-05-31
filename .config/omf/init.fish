9c488486aa39b9d7857301bf0eff93ed5248a871
Qinghua Hong <xohozu@gmail.com>
2022-05-31 20:20:05 +0800

    Update init.fish

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -16,3 +16,11 @@ set fish_pager_color_progress cyan
 # homebrew M1
 [ -f /opt/homebrew/bin/brew ]; and set PATH /opt/homebrew/bin $PATH
 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+
+# rbenv
+status --is-interactive; and rbenv init - fish | source
+
+# jdk
+set -xg JAVA_HOME (/usr/libexec/java_home)
+set PATH $JAVA_HOME $PATH
+
